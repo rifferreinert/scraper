ae36217859ba2bed49b3a977c2a1a85b8235b991
Ben Riffer-Reinert <rifferreinert@uchicago.edu>
2013-08-16 21:06:24 -0500

    fixing bugs

--- scraper.py
+++ scraper.py
@@ -11,36 +11,39 @@ def soup_link(url, maxTry = 0):
     while True:
         try:
             page = urlopen(url)
-            return soup(page)
+            return BeautifulSoup(page)
         except urllib.error.HTTPError as e:
             if attempt == maxTry:
                 raise e
             else:
                 attempt += 1
-      
+        except ConnectionResetError as e:
+            if attempt == maxTry:
+                raise e
+            else:
+                attempt += 1
+     
     
 def soup_links(links, numThreads = 20, maxTry = 0):
     """takes a list of url strings and returns a map object of soups. second argument
     determines the number of strings used."""
-    executor = ThreadPoolExecutor(max_workers = numThreads)
-    return executor.map(lambda x : soup_link(x, maxTry), links)
+    # executor = ThreadPoolExecutor(max_workers = numThreads)
+    return map(lambda x : soup_link(x, maxTry), links)
 
-class soup(BeautifulSoup):
-    def navigate(self, argsList):
-        """takes a list of arguments and navigates using them in order as arguments""" 
-        firstLoop = True
-        newSoup = self
-        for args in argsList:
-            name = None
-            attrs = None
-            text = None
-            if 'name' in args:
-                name = args['name']
-            if 'attrs' in args:
-                attrs = args['attrs']
-            if 'text' in args:
-                text = args['text']
-            newSoup = newSoup.find(name = name, attrs = attrs, text = text)
-            if not newSoup:
-                raise Exception('Could navigate to: name = {}, attrs = {}, text = {}'.format(name, attrs, text))
-        return newSoup
+def navigate(soup, argsList):
+    """takes a list of arguments and navigates using them in order as arguments""" 
+    firstLoop = True
+    for args in argsList:
+        name = None
+        attrs = None
+        text = None
+        if 'name' in args:
+            name = args['name']
+        if 'attrs' in args:
+            attrs = args['attrs']
+        if 'text' in args:
+            text = args['text']
+        soup = soup.find(name = name, attrs = attrs, text = text)
+        if not soup:
+            raise Exception('Could navigate to: name = {}, attrs = {}, text = {}'.format(name, attrs, text))
+    return soup
